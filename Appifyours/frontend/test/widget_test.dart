e7f47b1530270967c3138e0cf4b2acef1aefb1c5
Appifyours <appifyours01@gmail.com>
2026-08-01 11:17:26 +0530

    Update Appifyours/frontend/test/widget_test.dart

--- Appifyours/frontend/test/widget_test.dart
+++ Appifyours/frontend/test/widget_test.dart
@@ -1,13 +1,25 @@
 import 'package:flutter/material.dart';
+
 import 'package:flutter_test/flutter_test.dart';
+
 import 'package:appifyours/main.dart';
+
 void main() {
+
   testWidgets('App smoke test', (WidgetTester tester) async {
+
     await tester.pumpWidget(
+
       const MaterialApp(
+
         home: MyApp(),
+
       ),
+
     );
+
     expect(find.byType(MyApp), findsOneWidget);
+
   });
+
 }
